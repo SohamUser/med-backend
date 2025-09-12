f8b649d055a0b55282dcd424b94d7a9440e5c62b
SohamUser <sohamkulakrni@gmail.com>
2025-09-12 21:15:36 +0530

    added cors

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
 
-const app = express()
+const app = express();
+app.use(cors()); // allow requests from React Native
 
-app.listen(8080,"0.0.0.0",()=>{
-    console.log("running");
-})
+// Simple GET route
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from backend 👋" });
+});
+
+app.listen(8080, () => {
+  console.log("Server running on http://localhost:8080");
+});
